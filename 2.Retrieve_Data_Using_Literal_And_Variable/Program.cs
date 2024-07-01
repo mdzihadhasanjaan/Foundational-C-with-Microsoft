500d4366d92a58c377334b582196da2af4fd2a2f
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-01 22:14:41 +0300

    Code Added on no-2

--- 2.Retrieve_Data_Using_Literal_And_Variable/Program.cs
+++ 2.Retrieve_Data_Using_Literal_And_Variable/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This is a character literal
+Console.WriteLine('a');
+
+//This is integer literal
+Console.WriteLine(123);
+
+//This is floating point literal
+Console.WriteLine(0.25F);
+
+//This is double literal
+Console.WriteLine(2.045);
+
+//This is boolean literal
+Console.WriteLine(true);
+Console.WriteLine(false);
